837ef46f590d0e40f69a0054bfad58b851e3897b
Dicky Nugeraha <dicky.dian1@gmail.com>
2021-05-12 13:58:40 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
+/* eslint-disable quotes */
 const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
 
 const init = async () => {
  const server = Hapi.server({
   port: 5000,
-  host: "localhost",
+  // eslint-disable-next-line linebreak-style
+  host: process.env.NODE_ENV !== "production" ? "localhost" : "172.31.24.22",
   routes: {
    cors: {
     origin: ["*"],
